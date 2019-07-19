1a4a0720755b96289e33a8a3a2c24ad36bf65e68
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-07-19 11:40:25 +0300

    Add login form fuctionality Also keep the user details aslong and the user is logged in.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './redux/actions/authActions'
+
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
@@ -9,6 +13,16 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+
+// Check for token
+if(localStorage.jwtToken) {
+  // set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode user token and get user info and exp
+  const decode = jwt_decode(localStorage.jwtToken);
+  // Set user and is authenticated
+  store.dispatch(setCurrentUser(decode)); 
+}
 
 function App() {
   return (
