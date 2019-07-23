a6a67fb1faa3df989c1bc3947b251c362185233d
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-07-23 11:38:53 +0300

    Add the profile action buttons and also add delete account/profile button

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { getCurrentProfile } from '../../redux/actions/profileActions';
+import {
+	getCurrentProfile,
+	deleteAccount
+} from '../../redux/actions/profileActions';
 import Spinner from '../common/Spinner';
+import ProfileActions from './ProfileActions';
 
 class Dashboard extends Component {
 	componentDidMount() {
 		this.props.getCurrentProfile();
 	}
+	onDeleteClick = e => {
+		this.props.deleteAccount();
+	};
 	render() {
 		const { user } = this.props.auth;
 		const { profile, loading } = this.props.profile;
@@ -18,19 +25,34 @@ class Dashboard extends Component {
 		if (profile === null || loading) {
 			dashboardContent = <Spinner />;
 		} else {
-      // Check if logged in user has profile data
-			if(Object.keys(profile).length > 0) {
-        dashboardContent = <h4>Todo: Display profile</h4>
-      } else {
-        // User is logged in but has no profile
-        dashboardContent = <div>
-          <p className="lead text-muted">Welcome {user.name}</p>
-          <p>You have no profile please add one</p>
-          <Link to="/create-profile" className="btn btn-lg btn-info">
-            Create Profile
-          </Link>
-        </div>
-      }
+			// Check if logged in user has profile data
+			if (Object.keys(profile).length > 0) {
+				dashboardContent = (
+					<div>
+						<p className="lead text-muted">
+							Welcome <Link to={`/profile/${profile.handle}`}>{user.name}</Link>
+						</p>
+						<ProfileActions />
+						{/* TODO experience and education */}
+						<div style={{ marginBottom: '60px' }}>
+							<button onClick={this.onDeleteClick} className="btn btn-danger">
+								Delete My Account
+							</button>
+						</div>
+					</div>
+				);
+			} else {
+				// User is logged in but has no profile
+				dashboardContent = (
+					<div>
+						<p className="lead text-muted">Welcome {user.name}</p>
+						<p>You have no profile please add one</p>
+						<Link to="/create-profile" className="btn btn-lg btn-info">
+							Create Profile
+						</Link>
+					</div>
+				);
+			}
 		}
 		return (
 			<div className="dashboard">
@@ -48,7 +70,8 @@ class Dashboard extends Component {
 }
 
 Dashboard.propTypes = {
-	getCurrentProfile: PropTypes.func.isRequired,
+  getCurrentProfile: PropTypes.func.isRequired,
+  deleteAccount: PropTypes.func.isRequired,
 	auth: PropTypes.object.isRequired,
 	profile: PropTypes.object.isRequired
 };
@@ -58,4 +81,6 @@ const mapStateToProps = state => ({
 	auth: state.auth
 });
 
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
+export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(
+	Dashboard
+);
--- client/src/redux/actions/profileActions.js
+++ client/src/redux/actions/profileActions.js
@@ -3,7 +3,8 @@ import {
   GET_PROFILE,
   PROFILE_LOADING,
   GET_ERRORS,
-  CLEAR_CURRENT_PROFILE
+  CLEAR_CURRENT_PROFILE,
+  SET_CURRENT_USER
  } from '../types';
 
  // ProfileLoading
@@ -44,3 +45,17 @@ import {
   }
 }
 
+// Delete account and profile
+export const deleteAccount = () => dispatch => {
+  if(window.confirm('Are you sure? This can not be undone!')) {
+    axios.delete('/api/profile')
+      .then(res => dispatch({
+        type: SET_CURRENT_USER,
+        payload: {}
+      }))
+      .catch(error => dispatch({
+        type: GET_ERRORS,
+        payload: error.response.data
+      }))
+  }
+}
