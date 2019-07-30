eb1bc1181a96ecf61d0d53135a90cb23d61ec2c3
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-07-30 15:00:42 +0300

    Add posts display component

--- client/src/components/posts/Post.js
+++ client/src/components/posts/Post.js
@@ -3,15 +3,31 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import PostForm from './PostForm';
 import Spinner from '../common/Spinner';
+import { getPosts } from '../../redux/actions/postActions'
+import PostFeed from './PostFeed';
 
 class Post extends Component {
+
+  componentDidMount() {
+    this.props.getPosts();
+  }
 	render() {
+    const { posts, loading } = this.props.post;
+
+    let postContent;
+    if(posts === null || loading === true) {
+      postContent = <Spinner />
+    } else {
+      postContent = <PostFeed posts={posts} />
+    }
+
 		return (
       <div className="feed">
         <div className="container">
           <div className="row">
             <div className="col-md-12">
               <PostForm />
+              {postContent}
             </div>
           </div>
         </div>
@@ -20,4 +36,13 @@ class Post extends Component {
 	}
 }
 
-export default Post;
+Post.propTypes = {
+  post: PropTypes.object.isRequired,
+  getPosts: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+  post: state.post,
+})
+
+export default connect(mapStateToProps, { getPosts })(Post);
--- client/src/redux/actions/postActions.js
+++ client/src/redux/actions/postActions.js
@@ -1,18 +1,46 @@
-import axios from 'axios'
-import {
-  ADD_POST,
-  GET_ERRORS
- } from '../types';
+import axios from 'axios';
+import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING } from '../types';
 
- // Add Post
- export const addPost = (postData) => dispatch => {
-   axios.post('/api/posts', postData)
-    .then(res => dispatch({
-      type: ADD_POST,
-      payload: res.data
-    }))
-    .catch(error => dispatch({
-      type: GET_ERRORS,
-      payload: error.response.data
-    }))
- }
+// Add Post
+export const addPost = postData => dispatch => {
+	axios
+		.post('/api/posts', postData)
+		.then(res =>
+			dispatch({
+				type: ADD_POST,
+				payload: res.data
+			})
+		)
+		.catch(error =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: error.response.data
+			})
+		);
+};
+
+// Get Post
+export const getPosts = () => dispatch => {
+	dispatch(setPostsLoading());
+	axios
+		.get('/api/posts')
+		.then(res =>
+			dispatch({
+				type: GET_POSTS,
+				payload: res.data
+			})
+		)
+		.catch(error =>
+			dispatch({
+				type: GET_POSTS,
+				payload: null
+			})
+		);
+};
+
+//Set Posts loading
+export const setPostsLoading = () => {
+	return {
+		type: POST_LOADING
+	};
+};
