e025255bf2e2e933242e2bfb4b7d779f3744a7bb
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-08-02 12:18:31 +0300

    securing our secret keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://jude:jude@cluster0-oxmze.mongodb.net/test?retryWrites=true',
-    secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+	module.exports = require('./keys_prod');
+} else {
+	module.exports = require('./keys_dev');
+}
